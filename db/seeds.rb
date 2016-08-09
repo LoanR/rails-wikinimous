fafb007dd282c8e9f88f663279e2ec728261486e
Loan Robert <loan.rbrt@gmail.com>
2016-08-09 18:33:48 +0200

    unfinished wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  adjective = Faker::Hacker.adjective
+  noun = Faker::Hacker.noun
+  verb = Faker::Hacker.verb
+  ingverb = Faker::Hacker.ingverb
+  company = Faker::Company.name
+  par = Faker::Lorem.paragraph(9)
+  pars = Faker::Lorem.paragraph(18)
+  img = "https://source.unsplash.com/random/700x200"
+  name = adjective + " " + noun + " " + verb + (" the " || " a ") + noun
+  content = par + img + pars
+  article = Article.new(name: name, content: content)
+end
